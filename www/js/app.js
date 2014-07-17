61f82354b6be302fa0a674411854c2f96f278efc
Srdan Kvrgic <srdan@kvrgic.se>
2014-07-17 17:49:41 +0200

    Moved version-text and edited connect-text

--- www/js/app.js
+++ www/js/app.js
@@ -36,7 +36,7 @@ angular.module('starter', ['ionic'])
   $scope.share = function() {
     var userId = storage.getUserId();
     if (window.plugins && window.plugins.socialsharing) {
-      window.plugins.socialsharing.share("Copy-paste this code into the 'Enter code' menu in the application: " + userId, "Ionic feeder share code");
+      window.plugins.socialsharing.share("Copy-paste this code into the 'Connect Devices' -> 'Enter code' menu in the application: " + userId, "Ionic feeder share code");
     }
   }
 
