88775a39e332d0663bffbf9b2645edc0181e2dad
Srdan Kvrgic <srdan@kvrgic.se>
2014-08-24 22:29:41 +0200

    WIP on verbosity for charts

--- www/js/chartsController.js
+++ www/js/chartsController.js
@@ -6,4 +6,30 @@ var ChartsController = function($scope) {
     if(!percentage.drawn) { percentage.draw(); }
   });
 
+  $scope.histogramPeakTimes = function() {
+    var max = 0;
+    var maxIndex = 0;
+    for (var i = 0; i < histogram.hours.length; i++) {
+      var val = histogram.hours[i];
+      if (val > max) {
+        max = val;
+        maxIndex = i;
+      }
+    };
+    return max;
+  }
+
+  $scope.histogramPeakHour = function() {
+    var max = 0;
+    var maxIndex = 0;
+    for (var i = 0; i < histogram.hours.length; i++) {
+      var val = histogram.hours[i];
+      if (val > max) {
+        max = val;
+        maxIndex = i;
+      }
+    };
+    return maxIndex;
+  }
+
 }
