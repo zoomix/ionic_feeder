8a1417b952dd7ba954409d06749d68a1b65c939e
Srdan Kvrgic <srdan@kvrgic.se>
2014-12-23 15:11:39 +0100

    Hm. Flipping vertical vs horizintal. Coding on quarter of a brain here.

--- www/js/chartsController.js
+++ www/js/chartsController.js
@@ -1,15 +1,18 @@
 var ChartsController = function($scope) {
 
   $scope.spread = spread;
-  $scope.spreadTimes = [{key: 7,  name: "1 week" },
-                        {key: 14, name: "2 weeks"},
-                        {key: 28, name: "4 weeks"},
-                        {key: 56, name: "8 weeks"} ]
+  $scope.spreadTimes = [{key: 7,  name: "1 week" , vbars: ['Hidden']},
+                        {key: 14, name: "2 weeks", vbars: ['Bold','Hidden']},
+                        {key: 28, name: "4 weeks", vbars: ['','Bold','','Hidden']},
+                        {key: 56, name: "8 weeks", vbars: ['','','','Bold','','','','Hidden']}]
   $scope.spreadTime = $scope.spreadTimes[1];
   $scope.updateSpreadTime = function(newspreadTime) {
     $scope.spreadTime = newspreadTime;
     spread.nofDaysHistory = $scope.spreadTime.key;
     spread.update(function() {$scope.$apply();});
+  }
+  $scope.spreadVerticalBarWidth = function() {
+    return 100/$scope.spreadTime.vbars.length + "%";
   }
 
 
