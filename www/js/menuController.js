64e905a6f3412dd9ea6fc733f0fd5ff3fb82fa41
Srdan Kvrgic <srdan@kvrgic.se>
2015-10-19 20:25:45 +0200

    v1.1.1

--- www/js/menuController.js
+++ www/js/menuController.js
@@ -24,7 +24,7 @@ var MenuCtrl = function($scope, $ionicModal, $ionicPopup, $ionicSideMenuDelegate
     })
   }
 
-  $scope.version = "1.1.0";
+  $scope.version = "1.1.1";
 
   $scope.share = function() {
     var userId = storage.getUserId();
